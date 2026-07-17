d2954a677f03e6a77a83915e193259fde717bb65
Windtheking <santymendoza0106@gmail.com>
2026-07-16 21:08:48 -0500

    feat(scripts/comunidad.js ,  styles/comunidad.css ,  styles/index.css ,  templates/comunidad.html): Dinamical content injection update on proyect

--- scripts/comunidad.js
+++ scripts/comunidad.js
@@ -1,34 +1,48 @@
 var main_container_page_comunidad = document.getElementById("sections_1");
+var subsection_sections_1 = document.createElement("div");
+subsection_sections_1.classList.add('subsection_sections_1');
+// main_container_page_comunidad.append();
 
 document.addEventListener("DOMContentLoaded", function() {
-    main_container_page_comunidad.append(document.createElement("b").textContent = "append works")
 })
 
 var main_content_tittles = {
-        tittle_1: "estado de matrimonio",
-        tittle_2: "estado de viudas",
-        tittle_3: "estado de hombres",
-        tittle_4: "estado de jovenes"
+        tittle_1: "Estado de matrimonio",
+        tittle_2: "Estado de viudas",
+        tittle_3: "Estado de hombres",
+        tittle_4: "Estado de jovenes"
     }
 
 
 var main_content_content ={
-        content_1: "Prow scuttle parrel provost Sail ho shrouds spirits boom mizzenmast yardarm. Pinnace holystone mizzenmast quarter crow's nest nipperkin grog yardarm hempen halter furl. Swab barque interloper chantey doubloon starboard grog black jack gangway rutters.",
-        content_2: "Prow"
+        content_1: "Prow scuttle parrel provost Sail ho shrouds spirits boom mizzenmast yardarm. Pinnace holystone mizzenmast quarter crow's nest nipperkin grog yardarm hempen halter furl. Swab barque interloper chantey doubloon starboard grog black jack gangway rutters."
 }
 
 for (const property in main_content_tittles ) {
+    i = 0
+    while (i < 1){
+        main_container_page_comunidad.append(subsection_sections_1)
+        i++
+        console.log(i);
+    }  
+    //This creates a div for the tittle and content of the main content of the page and allows me to dinamically insert them into the main container of the page
     var div_creator = document.createElement("div")
     div_creator.classList.add('comunidad_estados')
     var tittle_text = document.createElement("h3")
     tittle_text.innerText = `${main_content_tittles[property]}`
-    div_creator.append(tittle_text) 
-    main_container_page_comunidad.append(div_creator)
+    div_creator.append(tittle_text)
+    subsection_sections_1.append(div_creator)
     console.log(`${property}: ${main_content_tittles[property]}`);
+    
+    for (const property in main_content_content ) {
+        var content_text = document.createElement("p")
+        content_text.innerText = `${main_content_content[property]}`
+        div_creator.append(content_text)
+        console.log(`${property}: ${main_content_content[property]}`);
+    }
 }
 
-// for (const property in main_content_tittles ) {
-//   main_
-// }
+
+
 
 console.log("Se ha cargado la página de comunidad correctamente."+ main_content_tittles.tittle_1);
